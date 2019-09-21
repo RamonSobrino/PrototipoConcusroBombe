42c7a15199c5e3147ee1c1893299bb43ac867542
Ramon Sobrino <ramonsobrinollorca@gmail.com>
2019-09-22 01:54:08 +0200

    Cambios en Combo

--- data/restaurante.js
+++ data/restaurante.js
@@ -3,32 +3,67 @@
     {
         'nombre':'Gastro bar el regreso',
         'coordenadas': [43.354046, -5.862735],
-        'tipo': 'burger'
+        'tipo': ['Internacional', 'Española']
     },
     {
         'nombre':'Vinoteo',
         'coordenadas': [43.366158, -5.853639],
-        'tipo': 'burger'
+        'tipo':  ['Mediteranea','Europea', 'Española']
     },
     {
         'nombre': 'Doña Concha',
         'coordenadas': [43.361571, -5.855499],
-        'tipo': 'burger'
+        'tipo': ['Mediteranea','Asiática', 'Española', 'Fusión']
     },
     {
         'nombre':'Casa Amparo',
         'coordenadas': [43.359848, -5.846363],
-        'tipo': 'burger'
+        'tipo':  ['Española']
     },
     {
         'nombre':'Las tablas del campillin',
         'coordenadas': [43.356555, -5.843377],
-        'tipo': 'burger'
+        'tipo':  ['Comtemporánea', 'Española']
     },
     {
         'nombre':'La viliella',
         'coordenadas': [43.363957, -5.844049],
-        'tipo': 'burger'
+        'tipo':  ['Española']
+    },
+    {
+        'nombre':'La quinta avenida',
+        'coordenadas': [43.364163, -5.847909],
+        'tipo':  ['Española']
+    },
+    {
+        'nombre':'La pigüeña',
+        'coordenadas': [43.363998, -5.843979],
+        'tipo':  ['Española']
+    },
+    {
+        'nombre':'Los tacos del Rey',
+        'coordenadas': [43.543289, -5.663832],
+        'tipo':  ['Mexicana']
+    },
+    {
+        'nombre':'Opera Cafe',
+        'coordenadas': [43.362617, -5.847537],
+        'tipo':  ['Comtemporánea', 'Española']
+    },
+    {
+        'nombre':'Bar tienda Mariluz',
+        'coordenadas': [43.357536, -5.848265],
+        'tipo':  ['Europa', 'Española']
+    },
+    {
+        'nombre':'La leyenda del gallo',
+        'coordenadas': [43.361477, -5.845347],
+        'tipo':  ['Mediteranea', 'Española']
+    },
+    {
+        'nombre':'El ferroviario',
+        'coordenadas': [43.364087, -5.844380],
+        'tipo':  ['Española']
     }
 
 ]
--- javascript/script.js
+++ javascript/script.js
@@ -2,19 +2,13 @@ var map;
 var poly;
 var markerPosReal;
 var circulos = [];
-var listenerMarker;
+var Markers = [];
 var puntuacionTotal = 0;
 var contadorSitios = 0;
+const tiposComida = new Set();
 
 
 function initMap() {
-
-    this.puntuacionTotal = 0;
-    this.contadorSitios = 0;
-
-
-
-
 
     var gastroMateo = new google.maps.LatLng(43.361365, -5.851818);
     var misOpciones = {
@@ -31,21 +25,27 @@ function initMap() {
 
     sitios.forEach((element, index) => {
         addMarker(new google.maps.LatLng(element.coordenadas[0], element.coordenadas[1]), index);
+        element.tipo.forEach(element => {
+            tiposComida.add(element);
+        });
     });
 
-    // this.listenerMarker = google.maps.event.addListener(map, 'click', function(event) {
-    //     situarMarcador(event.latLng);
-    // });
+    tiposComida.forEach(element => {
+        var x = document.getElementById("comboxBox");
+        var option = document.createElement("option");
+        option.text = element;
+        x.add(option);
+    });
 
-    // this.agregarFoto();
+
+
 
 }
 
-function situarMarcador(localizacion) {
-    var clickedLocation = new google.maps.LatLng(localizacion);
-    addMarker(localizacion);
-    map.panTo(localizacion);
+function filtrarSitios(selectedIndex){
+    var 
 }
+
 
 function addMarker(location, index) {
 
@@ -70,85 +70,10 @@ function addMarker(location, index) {
         infowindow.open(this.map, marker);
     });
 
+    makers.add(marker);
 
 }
 
-
-
-
-function puntuacion(distancia) {
-    if (distancia < 10000) {
-        return 100;
-    } else if (distancia < 20000) {
-        return 50;
-    } else if (distancia < 30000) {
-        return 25;
-    } else {
-        return 0;
-    }
-}
-function llenarCirculos() {
-    var i;
-    for (i = 0; i <= 30000; i += 10000) {
-        var circuloOptions = {
-            strokeColor: '#DCAAAC',
-            strokeOpacity: 0.4,
-            strokeWeight: 2,
-            fillColor: '#00FFFF',
-            fillOpacity: 0.10,
-            map: map,
-            center: marker.position,
-            radius: i
-        };
-        circulos.push(new google.maps.Circle(circuloOptions));
-    }
-
-    this.markerPosReal = new google.maps.Marker({
-        position: sitios[this.contadorSitios].coordenadas,
-        map: map,
-        draggable: false,
-        title: sitios[this.contadorSitios].titulo,
-        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    });
-
-    var dist = Math.round(google.maps.geometry.spherical.computeDistanceBetween(sitios[this.contadorSitios].coordenadas, marker.position));
-
-
-    var info = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">' + this.sitios[this.contadorSitios].titulo + '</h1>' +
-        '<div id="bodyContent">' +
-        "<p> " + this.sitios[this.contadorSitios].desc + "</p>" +
-        "<p> Distancia: " + dist + " metros</p>" +
-        "<p> Puntuacion: " + this.puntuacion(dist) + "</p>" +
-        '</div>' +
-        '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: info
-    });
-
-    infowindow.open(this.map, this.markerPosReal);
-
-    this.puntuacionTotal = this.puntuacionTotal + this.puntuacion(dist);
-    document.getElementById('area1').innerHTML = this.puntuacionTotal + " Puntos Totales";
-
-    document.getElementById('area2').innerHTML = this.puntuacion(dist) + " Puntos Parciales";
-
-    var polyOptions = {
-        strokeColor: '#FFFF00',
-        strokeOpacity: 1.0,
-        strokeWeight: 10
-    };
-
-    this.poly = new google.maps.Polyline(polyOptions);
-    this.poly.setMap(map);
-    path = poly.getPath();
-    path.push(sitios[this.contadorSitios].coordenadas);
-    path.push(marker.position);
-
-}
 
 function clearCirclesPath() {
     for (var i = 0; i < circulos.length; i++) {
