d06190afd5d4cfadbc8ea4b7032c728bad574ef9
Ramon Sobrino <ramonsobrinollorca@gmail.com>
2019-09-22 00:57:55 +0200

    Primera parte

---
+++ data/restaurante.js
@@ -0,0 +1,34 @@
+ var sitios = 
+[
+    {
+        'nombre':'Gastro bar el regreso',
+        'coordenadas': [43.354046, -5.862735],
+        'tipo': 'burger'
+    },
+    {
+        'nombre':'Vinoteo',
+        'coordenadas': [43.366158, -5.853639],
+        'tipo': 'burger'
+    },
+    {
+        'nombre': 'Doña Concha',
+        'coordenadas': [43.361571, -5.855499],
+        'tipo': 'burger'
+    },
+    {
+        'nombre':'Casa Amparo',
+        'coordenadas': [43.359848, -5.846363],
+        'tipo': 'burger'
+    },
+    {
+        'nombre':'Las tablas del campillin',
+        'coordenadas': [43.356555, -5.843377],
+        'tipo': 'burger'
+    },
+    {
+        'nombre':'La viliella',
+        'coordenadas': [43.363957, -5.844049],
+        'tipo': 'burger'
+    }
+
+]
