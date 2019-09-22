3aa03991d016c99b4cacfe50424e3e6f72b37b60
Carlos <cduarte_j@hotmail.com>
2019-09-22 02:56:54 +0200

    goggle places

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,10 @@
 var map;
+var markerSelected;
+
 var poly;
 var markerPosReal;
 var circulos = [];
-var Markers = [];
+var makers = [];
 var puntuacionTotal = 0;
 var contadorSitios = 0;
 const tiposComida = new Set();
@@ -36,14 +38,10 @@ function initMap() {
         option.text = element;
         x.add(option);
     });
-
-
-
-
 }
 
-function filtrarSitios(selectedIndex){
-    var 
+function filtrarSitios(selectedIndex) {
+
 }
 
 
@@ -54,25 +52,43 @@ function addMarker(location, index) {
         map: map
     });
 
-    google.maps.event.addListener(marker, 'click', function (event) {
-        var info = '<div id="content">' +
-            '<div id="siteNotice">' +
-            '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">' + sitios[index].nombre + '</h1>' +
-            '<div id="bodyContent">' +
-                        '</div>' +
-            '</div>';
+    google.maps.event.addListener(marker, 'click', function(event) {
+        markerSelected = marker;
+        var request = {
+            location: markerSelected.position,
+            radius: '5',
+            type: ['restaurant', 'cafe', 'bar']
+        };
 
-        var infowindow = new google.maps.InfoWindow({
-            content: info
-        });
-
-        infowindow.open(this.map, marker);
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
     });
 
-    makers.add(marker);
+    makers.push(marker);
 
 }
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        for (var i = 0; i < results.length; i++) {
+            let place = results[i];
+            let types = place.types;
+            if (types.indexOf('restaurant') !== -1 ||
+                types.indexOf('cafe') !== -1 ||
+                types.indexOf('bar') !== -1) {
+                new google.maps.InfoWindow({
+                    content: `<div id="content"><div id="siteNotice"></div>
+                        <h2 id="firstHeading" class="firstHeading">${place.name} (${place.rating})</h2>
+                        <div id="bodyContent">
+                            <p>${place.vicinity}</p>
+                        </div></div>`
+                }).open(map, markerSelected);
+            }
+        }
+    }
+}
+
 
 
 function clearCirclesPath() {
@@ -100,7 +116,7 @@ function quitarCirculosMarkersyLineas() {
 
 function okPuntuacion() {
     this.quitarCirculosMarkersyLineas();
-    this.listenerMarker = google.maps.event.addListener(map, 'click', function (event) {
+    this.listenerMarker = google.maps.event.addListener(map, 'click', function(event) {
         situarMarcador(event.latLng);
     });
     document.getElementById('confirmarButton').removeAttribute('disabled');
@@ -125,5 +141,3 @@ function agregarFoto() {
         "<h4> Imagen número " + (this.contadorSitios + 1) + "</h4>" +
         "<img src=\"img/" + this.sitios[this.contadorSitios].img + "\" >";
 }
-
-
