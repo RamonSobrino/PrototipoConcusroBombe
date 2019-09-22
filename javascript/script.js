e586597a611d67929cf56a7e40b48923e4924553
karolmc02 <karolmc02@gmail.com>
2019-09-22 13:10:43 +0200

    estilos

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,7 @@
 var map;
 var markerSelected;
 
-var poly;
-var markerPosReal;
-var circulos = [];
 var markers = [];
-var puntuacionTotal = 0;
-var contadorSitios = 0;
 const tiposComida = new Set();
 
 function initMap() {
@@ -14,10 +9,10 @@ function initMap() {
     var gastroMateo = new google.maps.LatLng(43.361365, -5.851818);
     var misOpciones = {
         center: gastroMateo,
-        zoom: 12,
+        zoom: 15,
         streetViewControl: false,
         disableDefaultUI: true,
-        minZoom: 6,
+        minZoom: 14,
         maxZoom: 19,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     };
@@ -36,35 +31,35 @@ function initMap() {
 
 
 function agregarMarcadores(sitiosFiltrados) {
-    sitiosFiltrados.forEach((element, index) => {
-        addMarker(new google.maps.LatLng(element.coordenadas[0], element.coordenadas[1]), index);
+    sitiosFiltrados.forEach(element => {
+        addMarker(new google.maps.LatLng(...element.coordenadas));
         element.tipo.forEach(element => {
             tiposComida.add(element);
         });
     });
 }
 
-function filtrarSitios(selectedIndex) {
+function filtrarSitios(tipoComida) {
 
     this.markers.forEach(element => {
         element.setMap(null);
     })
 
     var sitiosFiltrados = sitios.filter(element => {
-        return element.tipo.includes(selectedIndex)
+        return tipoComida == 'Todas' || element.tipo.includes(tipoComida)
     })
     agregarMarcadores(sitiosFiltrados);
 }
 
 
-function addMarker(location, index) {
+function addMarker(location) {
 
     let marker = new google.maps.Marker({
         position: location,
         map: map
     });
 
-    google.maps.event.addListener(marker, 'click', function(event) {
+    google.maps.event.addListener(marker, 'click', function (event) {
         markerSelected = marker;
         var request = {
             location: markerSelected.position,
@@ -80,7 +75,6 @@ function addMarker(location, index) {
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results);
         for (var i = 0; i < results.length; i++) {
             let place = results[i];
             let types = place.types;
@@ -89,7 +83,7 @@ function callback(results, status) {
                 types.indexOf('bar') !== -1) {
                 new google.maps.InfoWindow({
                     content: `<div id="content"><div id="siteNotice"></div>
-                        <h2 id="firstHeading" class="firstHeading">${place.name} (${place.rating})</h2>
+                        <h5>${place.name} (${place.rating})</h5>
                         <div id="bodyContent">
                             <p>${place.vicinity}</p>
                         </div></div>`
@@ -97,57 +91,4 @@ function callback(results, status) {
             }
         }
     }
-}
-
-
-
-function clearCirclesPath() {
-    for (var i = 0; i < circulos.length; i++) {
-        circulos[i].setMap(null);
-    }
-    circulos = [];
-}
-
-function confirmar() {
-
-    this.llenarCirculos();
-
-    document.getElementById('confirmarButton').setAttribute('disabled', true);
-    document.getElementById('okButton').removeAttribute('disabled');
-    google.maps.event.removeListener(this.listenerMarker);
-}
-
-function quitarCirculosMarkersyLineas() {
-    this.clearCirclesPath();
-    this.marker.setMap(null);
-    this.markerPosReal.setMap(null);
-    this.poly.setMap(null);
-}
-
-function okPuntuacion() {
-    this.quitarCirculosMarkersyLineas();
-    this.listenerMarker = google.maps.event.addListener(map, 'click', function(event) {
-        situarMarcador(event.latLng);
-    });
-    document.getElementById('confirmarButton').removeAttribute('disabled');
-    document.getElementById('okButton').setAttribute('disabled', true);
-
-    this.contadorSitios = this.contadorSitios + 1
-
-    if (this.contadorSitios == this.sitios.length) {
-        alert("Juego acabado. Puntuacion: " + this.puntuacionTotal)
-        this.initMap();
-    } else {
-        this.agregarFoto();
-
-    }
-
-}
-
-function agregarFoto() {
-    document.getElementById('area2').innerHTML = "0 Puntos Parciales";
-
-    document.getElementById('imagen').innerHTML =
-        "<h4> Imagen número " + (this.contadorSitios + 1) + "</h4>" +
-        "<img src=\"img/" + this.sitios[this.contadorSitios].img + "\" >";
 }
